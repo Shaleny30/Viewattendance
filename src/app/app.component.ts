b880127dc203f738e969eb1b9c697c0c82250bef
Shaleny30 <shalenyshaleny233@gmail.com>
2025-04-15 11:06:39 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ViewattendanceComponent } from './viewattendance/viewattendance.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ViewattendanceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/viewattendance/viewattendance.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewattendance',
+  imports: [],
+  templateUrl: './viewattendance.component.html',
+  styleUrl: './viewattendance.component.css'
+})
+export class ViewattendanceComponent {
+
+}
